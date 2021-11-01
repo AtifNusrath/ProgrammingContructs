95175a49991be0436e4e050d7b8460dfe807f54e
atif00 <mohammedatif90@gmail.com>
2021-11-01 10:13:51 +0530

    Refactor code to write function to get work hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -15,8 +15,7 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGEE_PER_HOUR=20;
 
-let empHrs=0;
-//let empCheck=Math.floor(Math.random()*10)%3;
+let empHrs;
 switch (empCheck){
     case IS_PART_TIME:
         empHrs=PART_TIME_HOURS;
@@ -29,5 +28,21 @@ switch (empCheck){
     
 }
 
-let empWage=empHrs*WAGEE_PER_HOUR;
-console.log("UC2-Emp Wage: "+empWage);
+let empWage=empHrs * WAGEE_PER_HOUR;
+console.log("Emp Wage: "+empWage);
+
+//UC3
+function getWorkingHours(eCheck){
+    switch (eCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;       
+    }
+}
+let eCheck=Math.floor(Math.random()*10)%3;
+empHrs=getWorkingHours(eCheck);
+empWage=empHrs*WAGEE_PER_HOUR;
+console.log("Hours: "+empHrs+" Emp Wage: "+empWage);
